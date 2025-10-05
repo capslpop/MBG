3dac2b37b2288d7e20241be4632822ddd2a7a706
johnw <jwbarry3@illinois.edu>
2025-10-05 14:43:43 -0500

    Add project files.

---
+++ MBG/Tests/tests.cpp
@@ -0,0 +1,18 @@
+﻿#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+
+#include <iostream>
+
+TEST_CASE("Simple Triangle") {
+
+    std::cout << "Hello TESTS!" << std::endl;
+
+    REQUIRE(1 + 1 == 2);
+}
+
+TEST_CASE("Simple Window") {
+
+    std::cout << "Hello Window!" << std::endl;
+
+    REQUIRE(1 + 1 == 2);
+}
