013aa20232c3a2c7b2012c15087098f4dd159b80
Eddie-Ouyang <eddie.ouyang@gmail.com>
2025-11-02 12:26:22 -0600

    input struct

--- MBG/MBG/OpenGL/Texture2D.hpp
+++ MBG/MBG/OpenGL/Texture2D.hpp
@@ -4,16 +4,48 @@
 
 class Texture2D {
 public:
-	Texture2D(size_t size_x, size_t size_y, TEXTURE_TYPE type, void* data = nullptr) {
+	struct Texture2DArgs {
+		GLsizei size_x;
+		GLsizei size_y;
+		TEXTURE_TYPE type;
+		void* data = nullptr;
+		GLenum wrap_s;
+		GLenum wrap_t;
+		GLenum min_filter;
+		GLenum mag_filter;
+		bool mipmap_en;
+	};
 
-		// Make sure to double check the byte alignment when sending data smaller than 4 bytes per x pixel length
+	Texture2D(Texture2DArgs input) {
+		type_ = tex_type_map[input.type].second;
+		glGenTextures(1, &ID);
+		glBindTexture(type_, ID);
 
+		glTexParameteri(type_, GL_TEXTURE_WRAP_S, input.wrap_s);
+		glTexParameteri(type_, GL_TEXTURE_WRAP_T, input.wrap_t);
+		glTexParameteri(type_, GL_TEXTURE_MIN_FILTER, input.min_filter);
+		glTexParameteri(type_, GL_TEXTURE_MAG_FILTER, input.mag_filter);
+
+		glTexImage2D(type_, 0, GL_RGB, input.size_x, input.size_y, 0, GL_RGB, GL_UNSIGNED_BYTE, input.data);
+
+		if (input.mipmap_en) glGenerateMipmap(type_);
+	}
+
+	void bind(int slot) {
+		glActiveTexture(GL_TEXTURE0 + slot);
+		glBindTexture(type_, ID);
+	}
+
+	void unbind() {
+		glBindTexture(type_, 0);
 	}
 
 	~Texture2D() {
-
+		glDeleteTextures(1, &ID);
 	}
 
-private:
 
+private:
+	unsigned int ID;
+	GLenum type_;
 };
