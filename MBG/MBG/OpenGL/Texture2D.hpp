d04fc0db6b91ca38a55a2a625cc922776fa15f13
Eddie-Ouyang <eddie.ouyang@gmail.com>
2025-11-09 15:24:40 -0600

    more struct for some reason

--- MBG/MBG/OpenGL/Texture2D.hpp
+++ MBG/MBG/OpenGL/Texture2D.hpp
@@ -4,31 +4,53 @@
 
 class Texture2D {
 public:
-	struct Texture2DArgs {
-		GLsizei size_x;
-		GLsizei size_y;
-		TEXTURE_TYPE type;
-		void* data = nullptr;
+	struct Texture2DParameters {
 		GLenum wrap_s;
 		GLenum wrap_t;
 		GLenum min_filter;
 		GLenum mag_filter;
+	};
+
+	struct Texture2DImage {
+		GLsizei size_x;
+		GLsizei size_y;
+		TEXTURE_TYPE type;
+		void* data = nullptr;
+	};
+
+	struct Texture2DArgs {
+		Texture2DImage image;
+		Texture2DParameters params;
 		bool mipmap_en;
 	};
 
-	Texture2D(Texture2DArgs input) {
-		type_ = tex_type_map[input.type].second;
+	Texture2D(Texture2DArgs input): type_(tex_type_map[input.image.type].second) {
 		glGenTextures(1, &ID);
+		setParameters(input.params);
+
+		if (input.image.data) glTexImage2D(type_, 0, tex_type_map[input.image.type].first, 
+										   input.image.size_x, input.image.size_x, 0, 
+										   tex_type_map[input.image.type].first, 
+										   GL_UNSIGNED_BYTE, input.image.data);
+		if (input.mipmap_en) glGenerateMipmap(type_);
+	}
+
+	void setParameters(Texture2DParameters input) {
 		glBindTexture(type_, ID);
 
 		glTexParameteri(type_, GL_TEXTURE_WRAP_S, input.wrap_s);
 		glTexParameteri(type_, GL_TEXTURE_WRAP_T, input.wrap_t);
 		glTexParameteri(type_, GL_TEXTURE_MIN_FILTER, input.min_filter);
 		glTexParameteri(type_, GL_TEXTURE_MAG_FILTER, input.mag_filter);
+	}
 
-		glTexImage2D(type_, 0, GL_RGB, input.size_x, input.size_y, 0, GL_RGB, GL_UNSIGNED_BYTE, input.data);
+	void setImage(Texture2DImage input) {
+		glBindTexture(type_, ID);
 
-		if (input.mipmap_en) glGenerateMipmap(type_);
+		if (input.data) glTexImage2D(type_, 0, tex_type_map[input.type].first, 
+									 input.size_x, input.size_y, 0, 
+									 tex_type_map[input.type].first, 
+									 GL_UNSIGNED_BYTE, input.data);
 	}
 
 	void bind(int slot) {
@@ -45,7 +67,9 @@ public:
 	}
 
 
+
 private:
 	unsigned int ID;
 	GLenum type_;
+	GLint color_type_;
 };
